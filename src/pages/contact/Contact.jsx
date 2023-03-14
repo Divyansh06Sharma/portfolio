598e0b453a18f817bf62c08c0c8c3ff69566b59d
Divyansh06Sharma <sharmadivyansh327@gmail.com>
2023-03-15 01:09:40 +0530

    contacts page update

--- src/pages/contact/Contact.jsx
+++ src/pages/contact/Contact.jsx
@@ -188,7 +188,7 @@ const Contact = () => {
                     </Fade>
                     <Reveal bottom>
                         <footer className='footer'>
-                            <p>copyright All Right Reserved Divyansh - 2022</p>
+                            <p>copyright All Rights Reserved to Divyansh - 2022</p>
                         </footer>
                     </Reveal>
                 </form>
